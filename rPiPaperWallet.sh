7ea5ae4beadb280fd5c81b4fc1411cd2aa790c38
Jeff Flowers <jflowers1974@users.noreply.github.com>
2017-04-27 17:45:49 -0700

    Logging - Changed logs to be more explicit

--- rPiPaperWallet.sh
+++ rPiPaperWallet.sh
@@ -30,9 +30,15 @@ if [ -f ~/rPiPaperWalletLog.txt ]
     echo "Allow me to create a log file for you..."
     echo "https://github.com/jflowers1974/rPiPaperWallets/" > rPiPaperWalletLog.txt
   fi
+echo "=======================================================" >> ~/rPiPaperWalletLog.txt
+echo "=======================================================" >> ~/rPiPaperWalletLog.txt
 uname -a >> ~/rPiPaperWalletLog.txt
 lsb_release -a >> ~/rPiPaperWalletLog.txt
 echo $BASH_RELEASE >> ~/rPiPaperWalletLog.txt
+echo "=======================================================" >> ~/rPiPaperWalletLog.txt
+echo "=======================================================" >> ~/rPiPaperWalletLog.txt
+
+# To be used a bit later - for benchmarking purpose and Timing
 rPiType=$(uname -a | awk '{ print $12 }')
 
 # OS Updating
@@ -40,7 +46,7 @@ clear
 echo "Updating the OS"
 sleep 2
 # Still using apt-get instead of the newer apt, as of Jessie apt-get seems to be more robust still
-sudo apt-get install -y build-essential automake autoconf libtool libgmp3-dev
+sudo apt-get install -y build-essential automake autoconf libtool libgmp3-dev zip unzip
 echo "INSTALLED: build-essential automake autoconf libtool libgmp3-dev" >> ~/rPiPaperWalletLog.txt
 sudo apt-get update && sudo apt-get upgrade -y
 echo "INSTALLED: Update && Upgrade" >> ~/rPiPaperWalletLog.txt
@@ -74,6 +80,7 @@ sudo -i
 echo "bcm2708-rng" | sudo tee --append /etc/modules
 echo "HRNGDEVICE=/dev/hwrng" | sudo tee --append /etc/default/rng-tools
 sudo service rng-tools restart
+echo "INSTALLED: Hardware RNG Restarted" >> ~/rPiPaperWalletLog.txt
 clear
 
 # Testing the Hardware Random Number Generator
@@ -81,16 +88,19 @@ clear
 # Use the type of computer and bench the HWRNG creating a set of speeds
 # Then compare these with the present
 # Also - one should really do this testing three times and take the average
-#
+# For example a raspberry pi 3 should take ~35 sec if everything is working ok
+# and >60 if the hardware randome number generator is not working
+# therefore, the 45.0 is a split - that could be fixed later.
 
-sleep 2 # doing this to calm down the computer before the next computation
+sleep 2 # doing this to calm down computer prior to these calculations
 
 rPiSpeed=$(sudo dd bs=128 count=1024 if=/dev/random of=/home/pi/random.txt |& awk '/copied/ { print $8 }')
 if [ $(echo $rPiSpeed'<'45.0 ];then
   echo "Hardware Random Number Generator Working Great" >> ~/rPiPaperWalletLog.txt
 else
   clear
-  echo "Hardware Random Number Generator Seems to be in error"
+  echo "Hardware Random Number Generator Seems to be in error!"
+  echo "ERROR: Hardware Randome Number Generator Tests FAILED!" >> ~/rPiPaperWalletLog.txt
   exit 1
 fi
 clear
@@ -104,12 +114,13 @@ if [ -d "supervanitygen" ];then
   git pull
   make
   cd ~
+  echo "UPDATED: supervanitygen.git" >> ~/rPiPaperWalletLog.txt
 else
   git clone https://github.com/klynastor/supervanitygen.git
   cd supervanitygen
   make
+  echo "INSTALLED: Bitcoin Vanity Generator" >> ~/rPiPaperWalletLog.txt
 fi
-echo "INSTALLED: Bitcoin Vanity Generator" >> ~/rPiPaperWalletLog.txt
 clear
 
 # Brain Wallet
@@ -120,9 +131,19 @@ if [ -d "brainwalletX.github.io" ];then
   cd ~/brainwalletX.github.io
   git pull
   cd ~
+  echo "UPDATED: brainwalletX.github.io" >> ~/rPiPaperWalletLog.txt
 else
   git clone https://github.com/brainwalletX/brainwalletX.github.io.git
+  echo "INSTALLED: brainwalletX.github.io" >> ~/rPiPaperWalletLog.txt
 fi
 cd ~
-echo "INSTALLED: Bitcoin Brain Wallet" >> ~/rPiPaperWalletLog.txt
 clear
+
+
+
+echo "=======================================================" >> ~/rPiPaperWalletLog.txt
+echo "=======================================================" >> ~/rPiPaperWalletLog.txt
+echo "END SCRIPT" >> ~/rPiPaperWalletLog.txt
+echo "=======================================================" >> ~/rPiPaperWalletLog.txt
+echo "=======================================================" >> ~/rPiPaperWalletLog.txt
+
